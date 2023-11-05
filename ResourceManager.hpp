18faceadfb736fc2b4c49bf0581077f8ce554b56
Jaku6M <j.mazurkiewicz13@wp.pl>
2023-11-05 18:00:54 +0100

    Pierwsza proba

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,27 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+    public:
+        ResourceManager() : resource_{} {}//lista inicjalizacyjna + konstruktor domyslny
+
+        ResourceManager(const ResourceManager& resource_copy) : resource_{resource_copy.resource_} {}//konstruktor kopiujacy
+
+        ResourceManager& operator=(const ResourceManager& resource_operator) 
+        {
+            if (this == &resource_operator) 
+            {
+                return *this; // Unikamy przypisania do samego siebie czyli 
+            }
+
+            resource_ = resource_operator.resource_;
+
+            return *this;
+        }
+
+        ~ResourceManager() {}//Destruktor
+
+        double get() {return resource_.get();}//metoda double get zwracajaca wynik z resource
+
+    private:
+        Resource resource_;//W ten sposob resource manager staje sie wlascicielem resource
 };
